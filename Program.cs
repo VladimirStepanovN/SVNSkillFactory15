c614cf9d478bc794dcd62da1e24166e5334caab4
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-30 00:47:31 +0500

    Задание 5.2.3.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿namespace SVNSkillFactory15
 {
     /// <summary>
-    /// Задание 5.2.2. Добавьте код из юнита 4.4 (Задание 4.4.2),
-    /// который получает данные пользователя, и исправьте метод ShowColor,
-    /// ему необходимо передать поле name кортежа. 
+    /// Задание 5.2.3. Измените код Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы").
+    /// Необходимо добавить переменную username, которая будет отображена в начале текста.
     /// </summary>
     internal class Program
     {
@@ -20,7 +19,7 @@
             string[] favcolors = new string[3];
             for (int i = 0; i < favcolors.Length; i++)
             {
-                Console.WriteLine($@"Напишите свой любимый цвет №{i + 1} на английском с маленькой буквы");
+                Console.WriteLine("{0} Напишите свой любимый цвет на английском с маленькой буквы", anketa.name);
                 favcolors[i] = ShowColor(anketa.name);
             }
             foreach (string favcolor in favcolors)
